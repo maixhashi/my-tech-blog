0ce43c9d7e714f60a9c6e0608e9ec890ef84d83a
maixhashi <maixhashi@gmail.com>
2025-07-21 07:23:24 +0900

    build/SSG時の`next/image`に関するエラー対策

--- src/app/_components/cover-image.tsx
+++ src/app/_components/cover-image.tsx
@@ -10,14 +10,12 @@ type Props = {
 
 const CoverImage = ({ title, src, slug }: Props) => {
   const image = (
-    <Image
+    <img
       src={src}
       alt={`Cover Image for ${title}`}
       className={cn("shadow-sm w-full", {
         "hover:shadow-lg transition-shadow duration-200": slug,
       })}
-      width={1300}
-      height={630}
     />
   );
   return (
